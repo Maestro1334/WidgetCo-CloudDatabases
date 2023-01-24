59617fca9a721dd1296fc5dfd07949661f8c8e12
Mattis <Mattis@DESKTOP-L4A4AP4>
2023-01-24 18:47:00 +0100

    Add project files.

---
+++ DOMAIN/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class User : BaseModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(Guid id, string email, string password)
+        {
+            Id = id;
+            Email = email;
+            Password = password;
+        }
+    }
+}
