e3b61e29bbced0dfa568e9677c7a43a03b384bda
Maestro1334 <622948@student.inholland.nl>
2023-01-24 19:48:43 +0100

    decapitalize domain/service folder names

---
+++ Domain/Models/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Order : BaseModel
+    {
+        Guid CustomerId { get; set; }
+        DateTime OrderDate { get; set; }
+        public virtual ICollection<OrderProduct> Products { get; set; }
+
+        decimal Total { get; set; }
+
+        OrderStatus Status { get; set; }
+        
+        public Order() { }
+
+        public Order(Guid id, Guid customerId, DateTime orderDate, ICollection<OrderProduct> products, decimal total, OrderStatus status)
+        {
+            Id = id;
+            CustomerId = customerId;
+            OrderDate = orderDate;
+            Products = products;
+            Total = total;
+            Status = status;
+        }
+    }
+}
---
+++ Domain/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Domain.Models
+{
+    public class Product : BaseModel
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public Product() { }
+
+        public Product(Guid id, string name, string description, string imageUrl)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+        }
+    }
+}
